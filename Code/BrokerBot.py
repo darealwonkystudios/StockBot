d51d6d2bce1fe4771314729f92e560b7566f94f0
TheRealZyBros <Azainusman13@gmail.com>
2025-08-11 17:58:56 -0400

    temp5

--- Code/BrokerBot.py
+++ Code/BrokerBot.py
@@ -284,7 +284,7 @@ def print_holdings():
 
         print(f"{contract.symbol}: {position} shares @ avg cost ${avg_cost:.2f}")
 
-Start(live=True)
+Start(live=False)
 
 print("TESTING!!!!")
 Buy("TSLA")
