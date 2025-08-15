1d9eaa41631c8a03244ec7f1a70427acb603bd20
TheRealZyBros <Azainusman13@gmail.com>
2025-08-15 11:12:39 -0400

    ver2

--- Code/BrokerBot.py
+++ Code/BrokerBot.py
@@ -314,8 +314,6 @@ def GetPriceOfTicker(ticker, timeout=5):
     return float(price)
 
 def Start(live=False):
-    ib.connect('127.0.0.1', 4002, clientId=1, timeout=30)
-    return
     if live:
         ib.connect('127.0.0.1', 4001, clientId=1, timeout=90)
     else:
